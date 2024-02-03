92e6f65d07a83eb9e2d71afac27286273a44d4b3
YBBMTAT <forsterite_0829_my@yahoo.co.jp>
2024-02-04 00:55:14 +0900

    新規登録時デフォルトで設定状態を生成する

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,4 +5,12 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
   has_many :timers
   has_one :usersetting
+
+  after_create :create_default_usersetting
+
+  private
+  def create_default_usersetting
+    # 新しい Usersetting レコードを作成し、関連付けられた User レコードの ID を user_id に設定する
+    build_usersetting(configuration_state: true, countdown_time: 300, user_id: self.id).save
+  end
 end
