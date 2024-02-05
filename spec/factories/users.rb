75a75c1d8254c85f725f096d4d3cf972f7ae8328
YBBMTAT <forsterite_0829_my@yahoo.co.jp>
2024-02-05 22:39:01 +0900

    userテストコードベース

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,8 +1,8 @@
 FactoryBot.define do
   factory :user do
-    nickname              {'test'}
-    email                 {'test@example'}
-    password              {'a12345'}
+    nickname              {Faker::Name.initials(number: 6)}
+    email                 {Faker::Internet.email}
+    password { Faker::Internet.password(min_length: 6, max_length: 20, mix_case: true, special_characters: false) }
     password_confirmation {password}
   end
 end
